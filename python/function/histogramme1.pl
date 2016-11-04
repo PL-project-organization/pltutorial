f45527eabc17962f8e994298a639a784c2416076
Dominique <dominique.revuz@gmail.com>
2016-11-04 16:37:40 +0100

    work

--- python/function/histogramme1.pl
+++ python/function/histogramme1.pl
@@ -1,6 +1,7 @@
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=Dominique Revuz 
 title=histogramme de notes
+name=histogramme de notes
 tag=function|list|branching
 template=/python/template
 text==
@@ -8,19 +9,20 @@ text==
 # Histogramme des Notes 
 
 Lire sur une première ligne le nombre de notes N, puis sur la ligne suivante les N notes en question.
-'''
-10
-12.3 14.5 17.8 18 11.6 13.3 16.7 7
-'''
-La fonction printHistoListes une liste de notes comprises dans chaque intervals de longeur 2 entre 0 et 20. 
-'''
-[7]
-[11.6]
-[12.3, 13.3]
-[14.5]
-[17.8, 16.7]
-[18]
-'''
+
+	'''
+	10
+	12.3 14.5 17.8 18 11.6 13.3 16.7 7
+	'''
+La fonction **printHistoListes** affiche une liste de notes comprises dans chaque intervals de longeur 2 entre 0 et 20. 
+	'''
+	[7]
+	[11.6]
+	[12.3, 13.3]
+	[14.5]
+	[17.8, 16.7]
+	[18]
+	'''
 ==
 
 pltest==
